6ebf827afc59f6ab2716f4acc5a9640eabf3874a
jihwan <jihwan.dev@gmail.com>
2018-10-25 21:38:05 +0900

    Update py_discord/pubg_search_bot.py
    중복되는부분 부분 함수화.

--- py_discord/pubg_search_bot.py
+++ py_discord/pubg_search_bot.py
@@ -6,6 +6,7 @@ import json
 client = discord.Client()
 
 pubg_key = ""
+
 def pubg_call(URI):
     headers = {"Authorization": "Bearer " + pubg_key, "Accept": "application/vnd.api+json"}
     response = requests.get(URI, headers = headers) 
@@ -18,6 +19,20 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+def get_game_type():
+    return ["solo","duo","squad"]
+
+def print_stats(stats, type):
+    solo_dict = stats.get(type)
+    items = stats.get(type).items()
+    if solo_dict.get("losses") != 0 or solo_dict.get("top10s") != 0 or solo_dict.get("wins") != 0   :
+        items = solo_dict.items()
+        msg = ""
+        for (key,value) in items :
+            msg += "\n" + str(key) + ":" + str(value)
+        return "============"+ type + " 전적검색 결과============" + msg
+
 
 @client.event
 async def on_message(message):
@@ -39,30 +54,14 @@ async def on_message(message):
         id = json.loads(response.text).get("data")[0].get("id")
 
         response = pubg_call('https://api.pubg.com/shards/steam/players/'+id+'/seasons/division.bro.official.pc-2018-01' )
-        Stats = json.loads(response.text).get("data").get("attributes").get("gameModeStats")
-        squad_dict = Stats.get("squad")
-        if squad_dict.get("losses") != 0 or squad_dict.get("top10s") != 0 or squad_dict.get("wins") != 0   :
-            items = squad_dict.items()
-            msg = ""
-            for (key,value) in items :
-                msg += "\n" + str(key) + ":" + str(value)
-            await client.send_message(message.channel, "============Squad 전적검색 결과============" + msg)
-        duo_dict = Stats.get("duo")
-        if duo_dict.get("losses") != 0 or duo_dict.get("top10s") != 0 or duo_dict.get("wins") != 0   :
-            items = duo_dict.items()
-            msg = ""
-            for (key,value) in items :
-                msg += "\n" + str(key) + ":" + str(value)
-            await client.send_message(message.channel, "============Duo 전적검색 결과============" + msg)
-
-        solo_dict = Stats.get("duo")
-        items = Stats.get("solo").items()
-        if solo_dict.get("losses") != 0 or solo_dict.get("top10s") != 0 or solo_dict.get("wins") != 0   :
-            items = solo_dict.items()
-            msg = ""
-            for (key,value) in items :
-                msg += "\n" + str(key) + ":" + str(value)
-            await client.send_message(message.channel, "============Solo 전적검색 결과============" + msg)
+        stats = json.loads(response.text).get("data").get("attributes").get("gameModeStats")
+        if stats:
+            game_types = get_game_type()
+            for current_game_type in game_types:
+                result = str(print_stats(stats, current_game_type)) 
+                if result != "None" : 
+                    await client.send_message(message.channel, result)
+            
 
 client.run('')
 
